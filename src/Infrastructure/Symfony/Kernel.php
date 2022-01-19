6d0355d797d41422c3fc186918ddd6bd3eb9a30a
imikado@gmail.com <imikado@gmail.com>
2022-01-19 22:45:13 +0100

    Migration clean architecture

--- src/Infrastructure/Symfony/Kernel.php
+++ src/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Infrastructure\Symfony\Kernel;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
